42d14afe2015adedd5b7199e9344210b52f2d5ed
karumujjidimbuvenkataakshay-byte <karumujjidimbuvenkataakshay@gmail.com>
2025-12-18 18:19:04 +0530

    Add files via upload

---
+++ email.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
